79900f3b3f65e28ebef3bd5cfa914c18de6355ff
obol007 <obol007@gmail.com>
2020-06-05 19:19:10 +0200

    controller, service, repository

---
+++ src/main/java/pl/obol/spring/api/PersonController.java
@@ -0,0 +1,31 @@
+package pl.obol.spring.api;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.obol.spring.model.Person;
+import pl.obol.spring.service.PersonService;
+
+import java.util.List;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+   @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+       return personService.getAllPeople();
+    }
+}
---
+++ src/main/java/pl/obol/spring/dao/FakePersonDataAccessService.java
@@ -0,0 +1,25 @@
+package pl.obol.spring.dao;
+
+import org.springframework.stereotype.Repository;
+import pl.obol.spring.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao {
+
+    private static List<Person> DB = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+}
